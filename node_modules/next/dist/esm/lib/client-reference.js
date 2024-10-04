32c0aa2f0c54769b586b7566f686b1df5bcf01d3
Ndiaga Ndiaye <njaga01@outlook.fr>
2024-10-04 20:54:00 +0000

    Suppression de node_modules de l'historique Git

--- node_modules/next/dist/esm/lib/client-reference.js
+++
@@ -1,6 +0,0 @@
-export function isClientReference(mod) {
-    const defaultExport = (mod == null ? void 0 : mod.default) || mod;
-    return (defaultExport == null ? void 0 : defaultExport.$$typeof) === Symbol.for("react.client.reference");
-}
-
-//# sourceMappingURL=client-reference.js.map
