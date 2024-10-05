91d8667f312b47aff7bbc5469af5b4b9f2eb1283
Ndiaga Ndiaye <njaga01@outlook.fr>
2024-10-05 02:08:02 +0000

    Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
